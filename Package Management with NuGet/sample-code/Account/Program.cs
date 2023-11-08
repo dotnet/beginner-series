2b47c3ade68b7e907ea675071d8491ef371cb33c
albarry4 <albarry@microsoft.com>
2023-11-08 13:40:23 -0800

    nuget sample code

---
+++ Package Management with NuGet/sample-code/Account/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json; 
+
+Account account = new Account
+{
+    Email = "james@example.com",
+    Active = true,
+    CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+    Roles = new List<string>
+    {
+        "User",
+        "Admin"
+    }
+};
+
+string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+Console.WriteLine(json);
---
+++ Package Management with NuGet/sample-code/AppLogger/Logger.cs
@@ -0,0 +1,9 @@
+﻿namespace AppLogger;
+
+public class Logger
+{
+    public void Log(string text)
+    {
+        Console.WriteLine(text);
+    }
+}
