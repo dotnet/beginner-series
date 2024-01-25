6420fa5baec7bab589dd15d72da4cf3467c46989
Daniel Roth <daroth@microsoft.com>
2024-01-24 17:27:01 -0800

    Add sample for working with data

---
+++ Frontend Web with Blazor/sample-code/working-with-data/BlazorMovieApp/Data/BlazorMovieAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorMovieApp;
+
+namespace BlazorMovieApp.Data
+{
+    public class BlazorMovieAppContext : DbContext
+    {
+        public BlazorMovieAppContext (DbContextOptions<BlazorMovieAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorMovieApp.Movie> Movie { get; set; } = default!;
+    }
+}
